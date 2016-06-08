88600212a262a1ce05407a9e4e7421f146c80676
Eric Dahl <eric@skeyelab.com>
2016-06-08 09:48:16 -0500

    some code

--- Gemfile
+++ Gemfile
@@ -1,1 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'dotenv'
 gem 'xmlstats'
+gem 'pry'
+gem 'fb_graph2'
+gem 'omniauth-facebook'
+gem "koala", "~> 2.2"
---
+++ TrollCraig.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
+######
+# Pull settings from the .env file.
+######
+Dotenv.load
+binding.pry
+@graph = Koala::Facebook::API.new(ENV["FB_TOKEN"], ENV["APP_SECRET"])
+
+
+today = Xmlstats.mlb_standing
+today.each do |team|
+  if team.last_name == "Cardinals"
+    # message = "@[#{ENV['CL_ID']}] Test"
+    # arguments_hash = {}
+    # arguments_hash["message"] = message
+
+    # @graph.rest_call("publish", arguments_hash)
+    @graph.put_wall_post("Today the Cardinals are #{team.games_back} games back!")
+  end
+end
