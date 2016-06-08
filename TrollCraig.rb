4e16ec64ac0ebef65d1c93e85ef448150e0866f7
Eric Dahl <eric@skeyelab.com>
2016-06-08 10:21:32 -0500

    added check for 1st place

--- TrollCraig.rb
+++ TrollCraig.rb
@@ -4,13 +4,26 @@ Bundler.require(:default)
 Dotenv.load
 @graph = Koala::Facebook::API.new(ENV["FB_TOKEN"], ENV["APP_SECRET"])
 today = Xmlstats.mlb_standing
-today.each do |team|
-  if team.last_name == "Cardinals"
-    # message = "@[#{ENV['CL_ID']}] Test"
-    # arguments_hash = {}
-    # arguments_hash["message"] = message
 
-    # @graph.rest_call("publish", arguments_hash)
-    @graph.put_wall_post("Today the Cardinals are #{team.games_back} games back!")
+stats = {}
+
+today.each do |team|
+
+  case team.last_name
+  when "Cubs"
+    stats["cubs_rank"] = team.ordinal_rank
+    stats["games_played"] = team.games_played
+    stats["cubs_won"] = team.won
+    stats["cubs_lost"] = team.lost
+  when "Cardinals"
+    stats["cards_games_back"] = team.games_back
   end
+
+end
+
+message = "Today the Cardinals are #{stats['cards_games_back']} games back. The Cubs are still in #{stats['cubs_rank']} place with a record of #{stats['cubs_won']} wins and #{stats['cubs_lost']} losses!"
+
+if stats["cubs_rank"] == "1st"
+  @graph.put_wall_post(message)
+  puts "Posted: #{message}"
 end
